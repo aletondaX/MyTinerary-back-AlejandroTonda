49db7243af7f461ed7f914eb851fa1c20537fa1c
aletondaX <gtrrsta@outlook.com>
2023-08-14 18:50:29 -0300

    Installed MongoDB, mongoose, created test DB

---
+++ models/Category.js
@@ -0,0 +1,15 @@
+import { Schema, Types, model } from "mongoose";
+
+let collection = "categories";
+
+let schema = new Schema({
+  name: {type: String, required: true},
+  color: {type: String, required: false},
+  // create_by: {type: Types.ObjectId, ref: "users"}
+}, {
+  timestamps: true
+})
+
+let Category = model(collection, schema);
+
+export default Category;
