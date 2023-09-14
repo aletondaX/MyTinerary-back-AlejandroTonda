bc3ea7c9420aa2e740e40a56c372753702925874
aletondaX <gtrrsta@outlook.com>
2023-09-14 19:33:13 -0300

    status 400 for validation errors

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,8 +2,8 @@ const validator = (schema) => (req, res, next) => {
   const validation = schema.validate(req.body, { abortEarly: true })
   
   if (validation.error) {
-    console.log(validation);
-    return res.json(validation.error);
+    console.log(validation.error);
+    return res.status(400).json(validation.error);
   }
   return next()
 }
