9903177705685d5ee120f823423045c18100e779
aletondaX <gtrrsta@outlook.com>
2023-12-17 19:04:22 -0300

    Minor fixes

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -1,18 +1,19 @@
+// import createHttpError from 'http-errors';
+// import createError from 'http-errors';
+
 const validator = (schema) => (req, res, next) => {
   const validation = schema.validate(req.body, { abortEarly: true })
   
   if (validation.error) {
     console.log(validation.error);
+    // createError(400, validation.error.details[0].message);
     return res.status(400).json(validation.error);
+    // return res.status(400).json({
+    //   success: false,
+    //   error: validation.error.details[0].message
+    // });
   }
   return next()
 }
 
 export default validator;
-
-// firstName: req.body.firstName,
-// lastName: req.body.lastName,
-// email: req.body.email,
-// password: req.body.password,
-// imgUrl: req.body.imgUrl,
-// country: req.body.country
